7526e668a8f9975156c4a0efa5b007e4015854d6
Khushal Subhash Borse <86597374+KhushalBorse2023@users.noreply.github.com>
2024-01-01 19:58:35 +0530

    Add files via upload

---
+++ Khushal/4. Median of Two Sorted Arrays/MedianOfTwoSortedArraySol.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size(),n2=nums2.size();
+        int total_length=n1+n2;
+        int left=(n1+n2+1)/2;
+        if(n1>n2) return findMedianSortedArrays(nums2,nums1);
+        int low=0;
+        int high=n1;
+        while(low<=high)
+        {
+            int mid1=low+(high-low)/2;
+            int mid2=left-mid1;
+            int l1=INT_MIN,l2=INT_MIN;
+            int r1=INT_MAX,r2=INT_MAX;
+            if(mid1-1>=0) l1=nums1[mid1-1];
+            if(mid2-1>=0) l2=nums2[mid2-1];
+            if(mid1<n1) r1=nums1[mid1];
+            if(mid2<n2) r2=nums2[mid2];
+            if(l1<=r2 and l2<=r1)
+            {
+                if(total_length%2==0)
+                {
+                    return (double)(max(l1,l2)+min(r1,r2))/2;
+                }
+                else
+                {
+                    return max(l1,l2);
+                }
+            }
+            else if(l1>r2)
+            {
+                high=mid1-1;
+            }
+            else
+            {
+                low=mid1+1;
+            }
+        }  
+        
+        
+        return 0;
+
+    }
+};
