e6122cc5afac7e67e3d72279cc1bbe93c44f5238
Jayesh Khairnar <71626566+jayeshkhairnar19@users.noreply.github.com>
2024-01-06 19:11:26 +0530

    Update Remove element.cpp

---
+++ JAYESH/Problem 8/Remove element.cpp
@@ -0,0 +1,14 @@
+//Problem Link = https://leetcode.com/problems/remove-element/description/
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+};
