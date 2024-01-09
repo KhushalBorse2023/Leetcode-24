661ae6dbe63d56ac5da38a172a3aae458eb200a0
Khushal Subhash Borse <86597374+KhushalBorse2023@users.noreply.github.com>
2024-01-09 22:38:19 +0530

    Create SerchInsertSol.cpp

---
+++ Khushal/35. Search Insert Position/SerchInsertSol.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        bool found=false;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(target==nums[i])
+            {
+                return i;
+        
+            }
+        break;
+        }
+        if(found==false)
+        {
+         nums.push_back(target);
+         sort(nums.begin(),nums.end());
+         for(int i=0;i<nums.size();i++)
+         {
+              if(nums[i]==target){
+                    found = true;
+                    return i;
+                }
+         }
+        }      
+        return 0;
+
+    }
+};
