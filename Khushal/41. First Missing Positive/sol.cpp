e2df743809c4477662218f79276fb18877d08ccc
Khushal Subhash Borse <86597374+KhushalBorse2023@users.noreply.github.com>
2024-01-18 00:05:57 +0530

    Update sol.cpp

--- Khushal/41. First Missing Positive/sol.cpp
+++ Khushal/41. First Missing Positive/sol.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_map<int ,bool>um;
+        for (auto it:nums)
+        {
+            um[it]=true;
+               
+        }
+        int i=1;
+        while(true)
+        {
+            if(um.find(i)==um.end())
+            { return i;}
+            i++;
+        }
+   return 0; }
+};
