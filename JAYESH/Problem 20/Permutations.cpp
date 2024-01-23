634564a1499bb71b8db928377acca35687c1f84d
Jayesh Khairnar <71626566+jayeshkhairnar19@users.noreply.github.com>
2024-01-23 19:17:43 +0530

    Update Permutations.cpp

--- JAYESH/Problem 20/Permutations.cpp
+++ JAYESH/Problem 20/Permutations.cpp
@@ -1,4 +1,5 @@
 //Problem Link = https://leetcode.com/problems/permutations/
+//Method 1
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
@@ -9,5 +10,32 @@ public:
            
         }while(next_permutation(nums.begin(),nums.end()));
       return ans;
+    }
+};
+
+//Method 2
+class Solution {
+private:
+    void solve( vector<vector<int>>& ans,int index,vector<int>nums){
+        //base case
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        for(int j = index; j<nums.size(); j++){
+            swap(nums[index],nums[j]);
+            solve(ans,index+1,nums);
+            // backtracking
+            swap(nums[index],nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(ans,index,nums);
+        return ans;
+        
     }
 };
