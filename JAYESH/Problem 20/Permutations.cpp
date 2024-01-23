ab23e290516abee1e3978e53264702a7aad58098
Jayesh Khairnar <71626566+jayeshkhairnar19@users.noreply.github.com>
2024-01-23 18:40:14 +0530

    Update Permutations.cpp

---
+++ JAYESH/Problem 20/Permutations.cpp
@@ -0,0 +1,13 @@
+//Problem Link = https://leetcode.com/problems/permutations/
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        do{
+            ans.push_back(nums);
+           
+        }while(next_permutation(nums.begin(),nums.end()));
+      return ans;
+    }
+};
