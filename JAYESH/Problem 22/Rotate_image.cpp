5415220a42545f6ea5dc13f41c974e85fa0f84fb
Jayesh Khairnar <71626566+jayeshkhairnar19@users.noreply.github.com>
2024-01-25 23:36:10 +0530

    Update Rotate_image.cpp

--- JAYESH/Problem 22/Rotate_image.cpp
+++ JAYESH/Problem 22/Rotate_image.cpp
@@ -1,39 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-    // Declare and initialize a 3x3 matrix
-    vector<vector<int>> matrix(3, vector<int>(3));
-
-    // Input matrix elements
-    cout << "Enter the matrix (3x3):" << endl;
-    for(int i = 0; i < 3; ++i) {
-        for(int j = 0; j < 3; ++j) {
-            cin >> matrix[i][j];
-        }
-    }
-
-    // Display the original matrix
-    cout<<endl;
+//Problem link = https://leetcode.com/problems/rotate-image/
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+    int Row = matrix.size();
+    int Col = matrix.size();      
     reverse(matrix.begin(), matrix.end());
-    cout<<"Reverse matrix"<<endl;
-    for(int i = 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    for(int i = 0; i<3; i++){
-        for(int j = i; j<3; j++){
-            swap(matrix[i][j],matrix[j][i]);
-        }
-        cout<<endl;
-    }
-    cout<<"Transpose matrix"<<endl;
-    for(int i = 0; i<3; i++){
-        for(int j = 0; j<3; j++){
-            cout<<matrix[i][j]<<" ";
-        }
-        cout<<endl;
+    for (int i = 0; i < Row; ++i) {
+        for (int j = i; j < Col; ++j)
+            swap(matrix[i][j], matrix[j][i]);
     }
 }
+
+    
+};
