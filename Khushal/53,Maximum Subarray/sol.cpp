c67413f33ea2fcf682e85848c0ff9908c5de642e
Khushal Subhash Borse <86597374+KhushalBorse2023@users.noreply.github.com>
2024-01-27 23:37:44 +0530

    Create sol.cpp

---
+++ Khushal/53,Maximum Subarray/sol.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+               int sum=0;
+        int ans=INT_MIN;
+for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(sum<nums[i])
+            {
+                sum=nums[i];
+            }
+            ans=max(ans,sum);
+        }
+    return ans;
+    }
+};
