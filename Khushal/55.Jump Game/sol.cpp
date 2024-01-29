00a1700f9e44fa276c38eb9c82c7be8bcf3ee2fe
Khushal Subhash Borse <86597374+KhushalBorse2023@users.noreply.github.com>
2024-01-29 20:16:47 +0530

    Create sol.cpp

---
+++ Khushal/55.Jump Game/sol.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int minJump=0;
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            minJump++;
+            if(nums[i]>=minJump)
+            {
+                minJump=0;
+            }
+        }
+        if(minJump==0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
