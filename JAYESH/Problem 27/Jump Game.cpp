85ef9cacfe78f8bce37b90f73f59c0808f442498
Jayesh Khairnar <71626566+jayeshkhairnar19@users.noreply.github.com>
2024-01-30 19:51:10 +0530

    Update Jump Game.cpp

---
+++ JAYESH/Problem 27/Jump Game.cpp
@@ -0,0 +1,16 @@
+//Problem link = https://leetcode.com/problems/jump-game/
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int index = nums.size()-1;
+        for(int i = nums.size()-1; i>=0; i--){
+            if(nums[i]+i>=index){
+                index = i;
+            }
+        }
+        if(index==0){
+            return true;
+        }
+        return false;
+    }
+};
