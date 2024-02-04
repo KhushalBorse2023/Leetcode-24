6dc8a6d21df7c848d1ffcbef29883ecfe55d0bac
Khushal Subhash Borse <86597374+KhushalBorse2023@users.noreply.github.com>
2024-02-04 21:06:34 +0530

    Create sol.cpp

---
+++ Khushal/66.Plus One/sol.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]<9)
+            {
+             digits[i]=digits[i]+1;
+                return digits;
+            }
+            digits[i]=0;
+
+        }
+     digits.push_back(0);
+     digits[0]=1;
+     return digits;
+
+    }
+};
