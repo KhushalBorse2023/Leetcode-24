077aa2dee293b61b10e13f7160b43bda2357976b
Jayesh Khairnar <71626566+jayeshkhairnar19@users.noreply.github.com>
2024-02-26 19:51:46 +0530

    Update and rename sol.cpp to Valid Parenthesis.cpp

---
+++ JAYESH/Problem 31/Valid Parenthesis.cpp
@@ -0,0 +1,22 @@
+// que - https://leetcode.com/problems/valid-parentheses/
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack();
+        for(char c : s.toCharArray()){
+            if(c=='('){
+                st.push(')');
+            }
+            else if(c=='['){
+                st.push(']');
+            }
+            else if(c=='{'){
+                st.push('}');
+            }
+           else if(st.isEmpty() || st.pop()!=c){
+               return false;
+           }
+        }
+        return st.isEmpty();
+        
+    }
+}
